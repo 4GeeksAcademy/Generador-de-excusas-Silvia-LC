acaf15447399f258c698eba8a7b0441848701a13
Silvia-LC <lopezsilvia36@gmail.com>
2025-05-13 16:04:45 +0000

    excusa

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+   function generateExcuse() {
+        let who = ['Mi jefe', 'Pedro Sánchez', 'Mi perro', 'Donald Trump'];
+        let action = ['se comió', 'se cagó en', 'lamió', 'besó'];
+        let what = ['el código', 'la ropa sucia', 'la tele', 'el laptop'];
+        let when = ['antes de entrar a trabajar', 'mientras dormía', 'en la iglesia', 'en el coche', 'en la ducha'];
+
+        let randomWho = who[Math.floor(Math.random() * who.length)];
+        let randomAction = action[Math.floor(Math.random() * action.length)];
+        let randomWhat = what[Math.floor(Math.random() * what.length)];
+        let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+        return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+    }
+
+    document.getElementById("excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
 };
